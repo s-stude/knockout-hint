c7347947dcea172cc29b98645ee24c067e324c0b
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-12-06 23:02:23 +0200

    Adding module

--- src/knockout-hint.js
+++ src/knockout-hint.js
@@ -1,21 +1,25 @@
-ko.bindingHandlers.hint = {
-    init:function (element, valueAccessor, allBindingsAccessor) {
-        var observable = valueAccessor();
+(function(win, $, ko){
 
-        if (observable()) {
-            $(element).removeClass('hidden');
-        } else {
-            $(element).addClass('hidden');
+    ko.bindingHandlers.hint = {
+        init:function (element, valueAccessor, allBindingsAccessor) {
+            var observable = valueAccessor();
+
+            if (observable()) {
+                $(element).removeClass('hidden');
+            } else {
+                $(element).addClass('hidden');
+            }
+        },
+
+        update:function (element, valueAccessor, allBindingsAccessor) {
+            var observable = valueAccessor();
+
+            if (observable()) {
+                $(element).removeClass('hidden');
+            } else {
+                $(element).addClass('hidden');
+            }
         }
-    },
+    };
 
-    update:function (element, valueAccessor, allBindingsAccessor) {
-        var observable = valueAccessor();
-
-        if (observable()) {
-            $(element).removeClass('hidden');
-        } else {
-            $(element).addClass('hidden');
-        }
-    }
-};
+})(window, jQuery, ko);
